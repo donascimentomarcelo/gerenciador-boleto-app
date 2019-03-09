5987d0ca29e640342d84e4ead7b46c228e076880
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2019-03-09 14:08:26 -0300

    Showing messages to make upload.

--- src/app/pages/users/user-ticket/user-ticket.component.ts
+++ src/app/pages/users/user-ticket/user-ticket.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewEncapsulation, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material';
+import { MAT_DIALOG_DATA, MatSnackBar, MatDialogRef } from '@angular/material';
 import { UsersService } from '../../services/users.service';
 
 @Component({
@@ -13,8 +13,10 @@ export class UserTicketComponent implements OnInit {
   file: any;
 
   constructor(
+    public dialogRef: MatDialogRef<UserTicketComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
-    private usersService: UsersService) { }
+    private usersService: UsersService,
+    private snackBar: MatSnackBar) { }
 
   ngOnInit() {
     console.log(this.data)
@@ -22,15 +24,54 @@ export class UserTicketComponent implements OnInit {
 
   public onFileInput(file: FileList): void {
     this.file = file[0];
+    const ext = this.file.name.split('.').pop();
+    if (this.file.size > 439940) {
+      this.snackBar.open('Envie um arquivo de até 432 KB.', 'Fechar', {
+        duration: 2000,
+      });
+      this.file = null;
+    }
+
+   if(ext !== 'pdf') {
+    this.snackBar.open('Selecione um arquivo .PDF.', 'Fechar', {
+      duration: 2000,
+    });
+    this.file = null;
+   }
   }
 
   public send(): void {
+
+    if (!this.file) {
+      this.snackBar.open('Selecione um arquivo válido.', 'Fechar', {
+        duration: 2000,
+      });
+      return;
+    }
+
     this.usersService.upload(this.file, this.data.user_id)
       .subscribe(resp => {
-        console.log(resp);
+        this.successMessage();
+        this.close();
       }, error => {
-        console.log(error);
+        this.errorMessage();
       });
+  }
+
+  public close(): void {
+    this.dialogRef.close();
+  }
+
+  public successMessage(): void {
+    this.snackBar.open('Upload realizado com sucesso', 'Fechar', {
+      duration: 2000,
+    });
+  }
+
+  public errorMessage(): void {
+    this.snackBar.open('Houve um erro ao realizar upload.', 'Fechar', {
+      duration: 2000,
+    });
   }
 
 }
